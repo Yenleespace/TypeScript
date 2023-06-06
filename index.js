87aa699102fba70c3b3040af2cbdafaf17d8c736
Yen Lee <yenny0104@gmail.com>
2023-06-06 15:25:10 -0400

    primitive types

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
+var 이름 = 'lee';
+var 나이 = 28;
+var 출신국가 = 'korea';
+var favorite = { song: 'pye life', singer: 'lil cherry' };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,15 @@
+let 이름 = 'lee';
+let 나이 = 28;
+let 출신국가 = 'korea';
+
+let favorite :{ song :string, singer :string } = { song : 'pye life', singer : 'lil cherry' }
+
+let project :{
+    member :string[],
+    days :number,
+    started :boolean,
+} = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
+}
