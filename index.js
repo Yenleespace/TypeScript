12e091cd972664933b78e7b247ed269cbebe6618
Yen Lee <yenny0104@gmail.com>
2023-06-06 16:13:15 -0400

    union type, any, unknown

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var 이름 = 'lee';
-var 나이 = 28;
-var 출신국가 = 'korea';
-var favorite = { song: 'pye life', singer: 'lil cherry' };
-var project = {
-    member: ['kim', 'park'],
-    days: 30,
-    started: true,
+var 회원들 = [1, '2', 3];
+var 오브젝트 = { a: '123' };
+var 나이 = 1;
+var user = 'kim';
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
 };
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
--- index.ts
+++ index.ts
@@ -1,15 +1,24 @@
-let 이름 = 'lee';
-let 나이 = 28;
-let 출신국가 = 'korea';
+let 회원들 :(number | string)[] = [1,'2',3]
+let 오브젝트 :{ a : (string | number) } = { a : '123'}
 
-let favorite :{ song :string, singer :string } = { song : 'pye life', singer : 'lil cherry' }
+let 나이 :unknown = 1;
 
-let project :{
-    member :string[],
-    days :number,
-    started :boolean,
-} = {
-    member : ['kim', 'park'],
-    days : 30,
-    started : true,
+let user :string = 'kim';
+let age :number | undefined = undefined;
+let married :boolean = false; 
+let 철수 :(string|number|undefined|boolean)[] = [user, age, married];
+
+let 학교 : {
+    score : (number|boolean)[],
+    teacher :string,
+    friend :string | string[],
 }
+
+={
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
+}
+
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
