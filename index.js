902300e0145605e014ca3e62d632bfcc1c2a4b96
Yen Lee <yenny0104@gmail.com>
2023-06-06 23:08:31 -0400

    function type

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-var 접니다;
-var 변수 = 'kim';
-var 자료 = {
-    name: 'kim'
-}; // 1. object value 값을 그대로 type 지정해줌 2. object 속성들에 모두 readonly 붙여줌 * object자료를 완전히 잠궈준다
-function 내함수(a) {
+//함수타입 표현 방법
+// function 함수(a :string) :number {
+// () => { return 10 } equal () => 10
+var 함수 = function (a) {
+    return 10;
+};
+var 회원정보 = {
+    name: 'kim',
+    age: 30,
+    plusOne: function (x) {
+        return x + 1;
+    },
+    changeName: function () {
+        console.log('안녕');
+    }
+};
+회원정보.plusOne(1);
+회원정보.changeName();
+var cutZero = function (x) {
+    return x.replace(/^0/, "");
+};
+var removeDash = function (x) {
+    var dash = x.replace(/-/g, '');
+    return parseFloat(dash);
+};
+function Final(a, func1, func2) {
+    var res1 = func1(a);
+    var res2 = func2(res1);
+    console.log(res2);
 }
-내함수(자료.name);
+Final('010-1111-2222', cutZero, removeDash);
--- index.ts
+++ index.ts
@@ -1,13 +1,58 @@
-let 접니다 :'대머리' | '솔로';
 
-const 변수  = 'kim';
+//함수타입 표현 방법
+// function 함수(a :string) :number {
 
-var 자료 = {
-    name : 'kim'
-} as const // 1. object value 값을 그대로 type 지정해줌 2. object 속성들에 모두 readonly 붙여줌 * object자료를 완전히 잠궈준다
+// }
 
-function 내함수(a : 'kim') {
+type 함수타입 = (a :string) => number // 무조건 string type을 넣고, return number type을 한다. 1. 함수타입은 () => {} 모양으로 해주고
 
+// () => { return 10 } equal () => 10
+
+let 함수 :함수타입 = function (a) { // 2. 함수표현식에만 type alias 사용가능
+    return 10
 }
 
-내함수(자료.name)
+// object 자료안의 함수 타입 지정(methods)
+type Member = {
+    name : string,
+    age : number,
+    plusOne : (x:number) => number,
+    changeName : () => void
+}
+
+let 회원정보 :Member = {
+    name : 'kim', 
+    age : 30,
+    plusOne (x){
+        return x + 1
+    },
+    changeName : () => {
+        console.log('안녕')
+    }
+}
+
+회원정보.plusOne(1);
+회원정보.changeName();
+
+///////////////////////////
+
+type cutType = (x :string) => string
+
+let cutZero :cutType = function (x) {
+    return x.replace(/^0/, "")
+}
+
+type removeType = (x :string) => number
+
+let removeDash :removeType = function (x) {
+    let dash = x.replace(/-/g,'')
+    return parseFloat(dash)
+}
+
+function Final(a, func1, func2) {
+    let res1 = func1(a);
+    let res2 = func2(res1)
+    console.log(res2)
+}
+
+Final('010-1111-2222', cutZero, removeDash)
