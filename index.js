2b95a8460203407970eac88a6ced1c539618a577
Yen Lee <yenny0104@gmail.com>
2023-06-07 13:37:55 -0400

    class type set

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-var 제목 = document.querySelector('#title');
-제목.innerHTML = '반갑소';
-var 사진 = document.querySelector('#image');
-if (사진 instanceof HTMLImageElement) {
-    사진.src = 'new.jpg';
-}
-var 링크 = document.querySelectorAll('.naver');
-링크.forEach(function (a) {
-    if (a instanceof HTMLAnchorElement) {
-        a.href = "kakao.com";
+var Car = /** @class */ (function () {
+    function Car(a, b) {
+        this.model = a;
+        this.price = b;
     }
-});
-for (var i = 0; i < 3; i++) {
-    var a = 링크[i];
-    if (a instanceof HTMLAnchorElement) {
-        a.href = "kakao.com";
+    Car.prototype.tax = function () {
+        return this.price / 10;
+    };
+    return Car;
+}());
+var car1 = new Car('소나타', 3000);
+console.log(car1); // { model : '소나타', price : 3000 }
+console.log(car1.tax()); // 300
+var Word = /** @class */ (function () {
+    function Word() {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var 숫자 = [];
+        var 문자 = [];
+        param.forEach(function (i) {
+            if (typeof i === 'string') {
+                문자.push(i);
+            }
+            else {
+                숫자.push(i);
+            }
+        });
+        this.num = 숫자;
+        this.str = 문자;
     }
-}
+    return Word;
+}());
+var obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num);
+console.log(obj.str);
--- index.ts
+++ index.ts
@@ -1,21 +1,43 @@
-let 제목 = document.querySelector('#title') as HTMLElement;
-    제목.innerHTML = '반갑소'
-
-let 사진 = document.querySelector('#image') 
-    if (사진 instanceof HTMLImageElement) {
-        사진.src = 'new.jpg'
+class Car {
+    model :string;
+    price :number;
+   constructor (a :string, b :number) {
+        this.model = a;
+        this.price = b
     }
 
-let 링크 = document.querySelectorAll('.naver') 
-    링크.forEach ((a)=>{
-        if (a instanceof HTMLAnchorElement) {
-            a.href = "kakao.com"
-        }
-    })
-
-    for (let i = 0; i < 3; i++) {
-        let a = 링크[i];  
-        if (a instanceof HTMLAnchorElement) {
-            a.href = "kakao.com"
-        }
+    tax(){
+        return this.price / 10
     }
+}
+
+
+let car1 = new Car('소나타', 3000)
+
+console.log(car1) // { model : '소나타', price : 3000 }
+console.log(car1.tax()) // 300
+
+class Word {
+    num;
+    str;
+    constructor(...param : (string | number)[]) {
+        let 숫자 :number[] = [];
+        let 문자 :string[] = [];
+
+        param.forEach((i)=>{
+            if (typeof i === 'string') {
+                문자.push(i)
+            } else {
+                숫자.push(i)
+            }
+        })
+
+        this.num = 숫자
+        this.str = 문자
+    }    
+}
+
+let obj = new Word('kim', 3, 5, 'park');
+
+console.log(obj.num) 
+console.log(obj.str)
