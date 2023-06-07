1e0b084942eb47497b4cdb432fd7d13368b1bffd
Yen Lee <yenny0104@gmail.com>
2023-06-06 23:53:47 -0400

    change HTML

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-//함수타입 표현 방법
-// function 함수(a :string) :number {
-// () => { return 10 } equal () => 10
-var 함수 = function (a) {
-    return 10;
-};
-var 회원정보 = {
-    name: 'kim',
-    age: 30,
-    plusOne: function (x) {
-        return x + 1;
-    },
-    changeName: function () {
-        console.log('안녕');
-    }
-};
-회원정보.plusOne(1);
-회원정보.changeName();
-var cutZero = function (x) {
-    return x.replace(/^0/, "");
-};
-var removeDash = function (x) {
-    var dash = x.replace(/-/g, '');
-    return parseFloat(dash);
-};
-function Final(a, func1, func2) {
-    var res1 = func1(a);
-    var res2 = func2(res1);
-    console.log(res2);
+var 제목 = document.querySelector('#title');
+제목.innerHTML = '반갑소';
+var 사진 = document.querySelector('#image');
+if (사진 instanceof HTMLImageElement) {
+    사진.src = 'new.jpg';
 }
-Final('010-1111-2222', cutZero, removeDash);
+var 링크 = document.querySelectorAll('.naver');
+링크.forEach(function (a) {
+    if (a instanceof HTMLAnchorElement) {
+        a.href = "kakao.com";
+    }
+});
+for (var i = 0; i < 3; i++) {
+    var a = 링크[i];
+    if (a instanceof HTMLAnchorElement) {
+        a.href = "kakao.com";
+    }
+}
--- index.ts
+++ index.ts
@@ -1,58 +1,21 @@
+let 제목 = document.querySelector('#title') as HTMLElement;
+    제목.innerHTML = '반갑소'
 
-//함수타입 표현 방법
-// function 함수(a :string) :number {
-
-// }
-
-type 함수타입 = (a :string) => number // 무조건 string type을 넣고, return number type을 한다. 1. 함수타입은 () => {} 모양으로 해주고
-
-// () => { return 10 } equal () => 10
-
-let 함수 :함수타입 = function (a) { // 2. 함수표현식에만 type alias 사용가능
-    return 10
-}
-
-// object 자료안의 함수 타입 지정(methods)
-type Member = {
-    name : string,
-    age : number,
-    plusOne : (x:number) => number,
-    changeName : () => void
-}
-
-let 회원정보 :Member = {
-    name : 'kim', 
-    age : 30,
-    plusOne (x){
-        return x + 1
-    },
-    changeName : () => {
-        console.log('안녕')
+let 사진 = document.querySelector('#image') 
+    if (사진 instanceof HTMLImageElement) {
+        사진.src = 'new.jpg'
     }
-}
 
-회원정보.plusOne(1);
-회원정보.changeName();
+let 링크 = document.querySelectorAll('.naver') 
+    링크.forEach ((a)=>{
+        if (a instanceof HTMLAnchorElement) {
+            a.href = "kakao.com"
+        }
+    })
 
-///////////////////////////
-
-type cutType = (x :string) => string
-
-let cutZero :cutType = function (x) {
-    return x.replace(/^0/, "")
-}
-
-type removeType = (x :string) => number
-
-let removeDash :removeType = function (x) {
-    let dash = x.replace(/-/g,'')
-    return parseFloat(dash)
-}
-
-function Final(a, func1, func2) {
-    let res1 = func1(a);
-    let res2 = func2(res1)
-    console.log(res2)
-}
-
-Final('010-1111-2222', cutZero, removeDash)
+    for (let i = 0; i < 3; i++) {
+        let a = 링크[i];  
+        if (a instanceof HTMLAnchorElement) {
+            a.href = "kakao.com"
+        }
+    }
