6d78fe5659c695b166da25f5dfa3c2ef8e8c9169
Yen Lee <yenny0104@gmail.com>
2023-06-09 10:41:56 -0400

    commit

--- typescript-todo-list/dist/index.js
+++ typescript-todo-list/dist/index.js
@@ -3,28 +3,59 @@ const btnSubmit = document.querySelector('.todo-btn');
 const inputTodo = document.querySelector('.todo-input');
 const formTodo = document.querySelector('.todo-form');
 const todoList = document.querySelector('.todo-list');
-const btnDeleteAll = document.querySelector('.todo-delete');
+const btnDeleteAll = document.querySelector('.todo-delete-all');
+// handle submit
 const handleSubmit = (e) => {
     e.preventDefault();
+    // create new todo object
     const newTodo = {
         id: Date.now(),
         todo: inputTodo.value,
         completed: false
     };
+    // todo save todo to local storage
     todos.push(newTodo);
+    // save to the local storage
+    saveTodos();
+    // append new todo Fn
     appendTodo(newTodo);
+    // reset input
     inputTodo.value = "";
 };
+// save todos
+const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
+;
+// new todos array
+const todos = JSON.parse(localStorage.getItem('todos') || '[]');
+console.log(todos);
+// append new todos to the DOM on start
+window.addEventListener('DOMContentLoaded', () => {
+    todos.forEach(todo => appendTodo(todo));
+});
+// append todo function
 const appendTodo = (newTodo) => {
     const newLi = document.createElement('li');
     const checkB = document.createElement('input');
-    checkB.type = "checkbox";
+    checkB.type = 'checkbox';
+    checkB.checked = newTodo.completed;
+    // add checkbox event listener
     checkB.addEventListener('change', () => {
-        console.log('checked');
+        console.log('Checked');
         newTodo.completed = checkB.checked;
+        // save todos
+        saveTodos();
     });
     newLi.append(newTodo.todo, checkB);
     todoList.prepend(newLi);
 };
-const todos = [];
+// add form event listener
 formTodo.addEventListener('submit', e => handleSubmit(e));
+// delete all
+const clearTodos = () => {
+    todos.length = 0;
+    saveTodos();
+    todoList.innerHTML = "";
+};
+btnDeleteAll.onclick = () => clearTodos();
--- typescript-todo-list/src/index.ts
+++ typescript-todo-list/src/index.ts
@@ -2,41 +2,74 @@ const btnSubmit = document.querySelector('.todo-btn') as HTMLButtonElement;
 const inputTodo = document.querySelector('.todo-input') as HTMLInputElement;
 const formTodo = document.querySelector('.todo-form') as HTMLFormElement;
 const todoList = document.querySelector('.todo-list') as HTMLElement;
-const btnDeleteAll = document.querySelector('.todo-delete') as HTMLButtonElement;
+const btnDeleteAll = document.querySelector('.todo-delete-all') as HTMLButtonElement;
 
-const handleSubmit = (e :Event) => {
+// handle submit
+const handleSubmit = (e: Event) => {
     e.preventDefault();
-
+    // create new todo object
     const newTodo :Todo= {
         id: Date.now(),
         todo: inputTodo.value,
         completed: false
     };
-
+    // todo save todo to local storage
     todos.push(newTodo);
+    // save to the local storage
+    saveTodos();
+
+    // append new todo Fn
     appendTodo(newTodo);
-    
-    inputTodo.value =  ""
+
+    // reset input
+    inputTodo.value = "";
+
+};
+// save todos
+const saveTodos = ()=>{
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
-const appendTodo = (newTodo :Todo) => {
+
+// new todo interface
+interface Todo {
+    id:number,
+    todo:string,
+    completed: boolean
+};
+
+// new todos array
+const todos: Todo[] = JSON.parse(localStorage.getItem('todos') || '[]');
+console.log(todos)
+// append new todos to the DOM on start
+window.addEventListener('DOMContentLoaded', ()=>{
+    todos.forEach(todo => appendTodo(todo));
+});
+
+// append todo function
+const appendTodo = (newTodo: Todo) => {
     const newLi = document.createElement('li');
     const checkB = document.createElement('input');
-    checkB.type = "checkbox";
-    
-    checkB.addEventListener('change',() => {
-        console.log('checked')
+    checkB.type = 'checkbox';
+    checkB.checked = newTodo.completed;
+    // add checkbox event listener
+    checkB.addEventListener('change', ()=>{
+        console.log('Checked');
         newTodo.completed = checkB.checked;
+        // save todos
+        saveTodos()
     })
     newLi.append(newTodo.todo, checkB);
     todoList.prepend(newLi);
 }
 
-interface Todo {
-    id:number,
-    todo :string,
-    completed :boolean
-}
 
-const todos: Todo[] = []
-
+// add form event listener
 formTodo.addEventListener('submit', e => handleSubmit(e));
+
+// delete all
+const clearTodos = () => {
+    todos.length = 0;
+    saveTodos();
+    todoList.innerHTML = "";
+}
+btnDeleteAll.onclick = () => clearTodos()
